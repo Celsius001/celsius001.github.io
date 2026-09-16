125f49fd5c170c6021c6bc65510b667a58ef73f4
DevvyE_yo <DevEchoX@gmail.com>
2026-09-15 21:12:12 -0400

    Integrate Firebase Auth for user initialization

--- friends/script.js
+++ friends/script.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, query, orderBy, onSnapshot, doc, setDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDj46RSodJ56rWwsxp9wh2x44hcZtBImxw",
@@ -12,6 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 let currentUser = "";
 let currentAvatar = "../favicon.ico";
@@ -61,19 +63,19 @@ document.getElementById('nav-chat').addEventListener('click', () => {
     window.location.href = '../chat/index.html';
 });
 
-function initUser() {
-    currentUser = localStorage.getItem('celsius_username') || "User_" + Math.floor(1000 + Math.random() * 9000);
+function initUser(user) {
+    currentUser = user.displayName || localStorage.getItem('celsius_username') || user.email?.split('@')[0] || "User";
     currentAvatar = localStorage.getItem('celsius_avatar') || "../favicon.ico";
     
     currentUsernameDisplay.textContent = currentUser;
     currentUserAvatarDisplay.src = currentAvatar;
 
-    registerUserPresence();
+    registerUserPresence(user.uid);
     listenToOnlineUsers();
 }
 
-function registerUserPresence() {
-    const userRef = doc(db, 'online_users', currentUser);
+function registerUserPresence(uid) {
+    const userRef = doc(db, 'online_users', uid);
     const sendHeartbeat = () => {
         setDoc(userRef, { 
             username: currentUser, 
@@ -145,4 +147,10 @@ function escapeHtml(str) {
     return (str || '').replace(/[&<>'"]/g, tag => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag));
 }
 
-initUser();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        initUser(user);
+    } else {
+        window.location.href = '../index.html';
+    }
+});
